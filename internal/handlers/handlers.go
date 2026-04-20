602323f174952269e4b1c178d0aa8c686eeb2656
Nick Chervov <nickchervoff@gmail.com>
2026-04-20 18:28:30 +0300

    added a few changes

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -52,7 +52,7 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(convertedData))
 }
