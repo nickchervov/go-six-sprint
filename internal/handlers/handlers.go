b765d8f7f1cbd8d18bab9e6e1d2045a9fc795c95
Nick Chervov <nickchervoff@gmail.com>
2026-04-20 18:30:23 +0300

    added some changes

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -16,7 +16,7 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write(resp)
 }
