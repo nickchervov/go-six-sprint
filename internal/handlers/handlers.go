170638c7ee4cb8ac5d50b07097f7c43f7bc36898
Nick Chervov <nickchervoff@gmail.com>
2026-04-20 18:11:00 +0300

    added a few changes

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -16,17 +16,12 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
 	w.Write(resp)
 }
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
-	resp, err := os.ReadFile("../index.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
 	r.ParseMultipartForm(10 << 20)
 	file, _, err := r.FormFile("myFile")
 	if file == nil {
@@ -59,6 +54,5 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write(resp)
 	w.Write([]byte(convertedData))
 }
